2f59aea5288677c8485eee27a4c4bac878546951
michalharcar <michalharcar@nextra.sk>
2013-08-11 15:18:53 +0200

    new file edited

--- Test/src/main/java/bla.java
+++ Test/src/main/java/bla.java
@@ -1,5 +1,16 @@
+import org.apache.http.impl.io.AbstractSessionOutputBuffer;
+import org.apache.http.params.HttpParams;
+
+import java.io.OutputStream;
+
 /**
  * Created by Michal on 11.8.2013.
  */
 public class bla {
+    AbstractSessionOutputBuffer asdsd = new AbstractSessionOutputBuffer() {
+        @Override
+        protected void init(OutputStream outstream, int buffersize, HttpParams params) {
+            super.init(outstream, buffersize, params);
+        }
+    };
 }
